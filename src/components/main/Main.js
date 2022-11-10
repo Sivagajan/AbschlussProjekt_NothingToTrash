6d3d1d5fbc32f55472138d2679755a7c04d9ebc7
supercode <getlucked@gmx.de>
2022-11-10 16:34:37 +0100

    main90% done

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -3,7 +3,7 @@ import "./Main.Modules.scss"
 const Main = () => {
     return (
         <main>
-            <section>
+            <section className="section1main">
                 <article className="articleleft">
                     <div>
                         <img src="./img/mennotebook1.png" alt="" />
@@ -17,7 +17,7 @@ const Main = () => {
                     <div>
                         <img className="middlepicture" src="./img/chairs2middle.png" alt="" />
                     </div>
-                    <h3>Verkaufen statt wegwerfen</h3>
+                    <h3>Verschenke und Schütze</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus faucibus egestas neque, quis nunc in turpis cursus eget.</p>
                 </article>
 
@@ -30,10 +30,54 @@ const Main = () => {
 
 
                     </div>
-                    <h3>Verkaufen statt wegwerfen</h3>
+                    <h3>Der Umwelt zuliebe</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus faucibus egestas neque, quis nunc in turpis cursus eget. </p>
                 </article>
 
+            </section>
+
+            <section className="section2main">
+                <article className="section2article1">
+                    <div className="section2a1d1">
+                        <img className="section2a1pic1" src="./img/section2pic1.png" alt="" />
+                        <img className="section2a1pic1shadow" src="./img/shadowpic1.png" alt="" />
+                        <img className="section2a1pic2" src="./img/großerkreis.png" alt="" />
+                        <img className="section2a1pic3" src="./img/ganzkleinerkreis.png" alt="" />
+                        <img className="section2a1pic4" src="./img/hochkantpunkte.png" alt="" />
+                    </div>
+                    <div className="section2a1d2">
+                        <h3>Ohne</h3>
+                        <h3>Limits</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus faucibus egestas neque, quis nunc in turpis cursus eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus faucibus egestas neque, quis nunc in turpis cursus eget.</p>
+                        <button>Zur Dokumentation</button>
+                    </div>
+                </article>
+
+                <article className="section2article2">
+                    <div className="section2a2d1">
+                        <img className="section2a2pic1" src="./img/section2pic2.png" alt="" />
+                        <img className="section2a1pic2shadow" src="./img/shadowpic2.png" alt="" />
+                        <img className="section2a2pic2" src="./img/mittelgroßerkreis.png" alt="" />
+                        <img className="section2a2pic3" src="./img/querpunkte.png" alt="" />
+                        <img
+                            className="section2a2pic4" src="./img/kleinerkreis.png" alt="" />
+                    </div>
+                    <div className="section2a2d2">
+                        <h3>Kenne deine</h3>
+                        <h3>Community</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus faucibus egestas neque, quis nunc in turpis cursus eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus faucibus egestas neque, quis nunc in turpis cursus eget.</p>
+                        <button>Zur Dokumentation</button>
+                    </div>
+                </article>
+
+            </section>
+
+            <section className="section3">
+                <h2>Von echten Menschen unterstützt</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus faucibus egestas neque, quis nunc in turpis cursus eget.</p>
+                <div>
+                    <img className="gruppenfoto" src="./img/gruppenfoto.png" alt="" />
+                </div>
             </section>
         </main>
     )
