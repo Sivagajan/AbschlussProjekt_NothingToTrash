d147ed531d905827538b065d85945f3331211fea
Sivagajan <sivagajan@gmail.com>
2022-11-14 12:09:06 +0100

    folders for buttons

--- src/components/buttons/startButton/StartButton.jsx
+++ src/components/buttons/startButton/StartButton.jsx
@@ -3,17 +3,16 @@ import { motion } from 'framer-motion'
 import LoginForm from '../../login&RegisterForm/LoginForm'
 import { useState } from 'react'
 
-const StartButton = (props) => {
+const StartButton = () => {
 
     const [open, setOpen] = useState(false)
-
 
     return(
         <>
             <LoginForm open={open} setOpen={setOpen}/>
             <motion.button className='startBtn' 
                 whileHover={{ scale: 1.2 }}whileTap={{scale: 0.8,}}
-            onClick={() => setOpen(true) && props.setbg(true)}> Starte jetzt! </motion.button>
+            onClick={() => setOpen(true)}> Starte jetzt! </motion.button>
         
         </>
     )
