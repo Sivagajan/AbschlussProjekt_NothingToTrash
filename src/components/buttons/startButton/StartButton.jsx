af253c495e1079a6334f6d282238ab65c9ce5b17
Sivagajan <sivagajan@gmail.com>
2022-11-14 15:12:33 +0100

    everything uptodate

--- src/components/buttons/startButton/StartButton.jsx
+++ src/components/buttons/startButton/StartButton.jsx
@@ -2,15 +2,15 @@ import './StartButton.Modules.scss'
 import { motion } from 'framer-motion'
 import LoginForm from '../../login&RegisterForm/LoginForm'
 import { useState } from 'react'
+import Userlogin from '../../../pages/userlogin/UserLogin'
 
 const StartButton = () => {
 
     const [open, setOpen] = useState(false)
-    console.log('start Button', props.setbg)
 
     return(
         <>
-            <LoginForm open={open} setOpen={setOpen}/>
+            <Userlogin open={open} setOpen={setOpen}/>
             <motion.button className='startBtn' 
                 whileHover={{ scale: 1.2 }}whileTap={{scale: 0.8,}}
             onClick={() => setOpen(true)}> Starte jetzt! </motion.button>
--- src/pages/userlogin/UserLogin.jsx
+++ src/pages/userlogin/UserLogin.jsx
@@ -2,36 +2,39 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import LoginForm from '../../components/login&RegisterForm/LoginForm'
 
-const SERVER = process.env.BACKEND_ADRESS
+const Userlogin = (props) => {
 
-const Userlogin = () => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const nav = useNavigate()
 
- /*    const login = async () => {
-        const fetchData = await fetch(`${SERVER}article`,{
+
+    const login = async () => {
+        const result = await fetch('http://localhost:9090/user/login',{
             method: 'POST',
             headers: {
                 'content-ype': 'application/json'
             },
             body: JSON.stringify({username, password})
         })
-        if(fetchData.status === 200) {
-            const data = await fetchData.json()
+        if(result.status === 200) {
+
+            const data = await result.json()
 
             localStorage.setItem('token', data.token)
             nav('/market')
         }
-    } */
+    }
+
+    console.log(username, password)
     
-    return(
+    return (props.open)?(
         <div className="container">
             <div>
-                <LoginForm /* login={login} */ setusername={setUsername} setpassword={setPassword}/>
+                <LoginForm login={login} setusername={setUsername} setpassword={setPassword}/>
             </div>
         </div>
-    )
+    ):""
 }
 
 export default Userlogin
