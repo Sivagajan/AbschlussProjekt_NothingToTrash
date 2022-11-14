98c1d665f1f034609093ae8e816b8616e780f45c
Sivagajan <sivagajan@gmail.com>
2022-11-14 11:52:06 +0100

    folders for buttons

---
+++ src/components/buttons/registerButton/RegisterButton.jsx
@@ -0,0 +1,8 @@
+import './RegisterButton.Modules.scss'
+
+const RegisterButton = () => {
+    return(
+        <button className='registerBtn'> Registriere Dich </button>
+    )
+}
+export default RegisterButton
