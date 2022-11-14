95c76a76821b6a8f69de5cf4f5407139f76b32d9
Magdalena Cale <107847566+MagdaCale@users.noreply.github.com>
2022-11-14 12:07:55 +0100

    commit

--- src/components/login&RegisterForm/LoginForm.jsx
+++ src/components/login&RegisterForm/LoginForm.jsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom'
 
 
 const LoginForm = (props) => {
-    return (props.open) ? (
+    return (props.open) ? 
+    (
         <div className='bgOpacity'>
             <motion.section className='container' 
             initial={{ scale: 0 }} animate={{ rotate: 360, scale: 1 }}
