71b856b3f6db508f863a68dc8a8d5e9f81547839
Magdalena Cale <107847566+MagdaCale@users.noreply.github.com>
2022-11-15 12:00:06 +0100

    navbar links

--- src/components/buttons/registerButton/RegisterButton.jsx
+++ src/components/buttons/registerButton/RegisterButton.jsx
@@ -1,8 +1,14 @@
 import './RegisterButton.Modules.scss'
+import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 const RegisterButton = () => {
     return(
-        <button className='registerBtn'> Registriere Dich </button>
+
+            <Link to={'/register'}><motion.button className='registerBtn' 
+                whileHover={{ scale: 1 }}
+                whileTap={{scale: 0.8}}
+            > Registriere Dich </motion.button></Link>
     )
 }
 export default RegisterButton
--- src/components/buttons/startButton/StartButton.jsx
+++ src/components/buttons/startButton/StartButton.jsx
@@ -1,6 +1,5 @@
 import './StartButton.Modules.scss'
 import { motion } from 'framer-motion'
-import LoginForm from '../../login&RegisterForm/LoginForm'
 import { useState } from 'react'
 import Userlogin from '../../../pages/userlogin/UserLogin'
 
@@ -12,7 +11,8 @@ const StartButton = () => {
         <>
             <Userlogin open={open} setOpen={setOpen}/>
             <motion.button className='startBtn' 
-                whileHover={{ scale: 1.2 }}whileTap={{scale: 0.8,}}
+                /* whileHover={{ scale: 1 }} */
+                whileTap={{scale: 0.8}}
             onClick={() => setOpen(true)}> Starte jetzt! </motion.button>
         
         </>
