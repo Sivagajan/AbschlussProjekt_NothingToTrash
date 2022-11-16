57415f3e22a97787797b022e6f323cb63295abeb
Magdalena Cale <107847566+MagdaCale@users.noreply.github.com>
2022-11-16 15:04:38 +0100

    jojojojoj

--- src/pages/productdetails/ProductDetails.jsx
+++ src/pages/productdetails/ProductDetails.jsx
@@ -14,21 +14,21 @@ const ProductDetails = () => {
             <Navbar />
             <section className={`${style.detailssec1} ${style.dflex}`}>
                 <img className={style.imgdetails} src="./img/tablet.jpg" alt="" />
-                <article>
-                    <h2>Couch</h2>
+                <article className={style.productInfo}>
+                    <h2>iPad</h2>
                     <p className={style.price}>45,00 EUR</p>
                     <div className={`${style.paddingbottom1} ${style.dflex}`}>
                         <div className={style.zustandmarke}>
-                            <p>Zustand</p>
-                            <p>Marke</p>
-                            <p>Lieferung</p>
-                            <p>Anzahl</p>
+                            <p className={style.pTag}>Zustand</p>
+                            <p className={style.pTag}>Marke</p>
+                            <p className={style.pTag}>Lieferung</p>
+                            <p className={style.pTag}>Anzahl</p>
                         </div>
-                        <div className={style.zustandmarke2}>
-                            <p>Zustand</p>
-                            <p>Marke</p>
-                            <p>Lieferung</p>
-                            <p>Anzahl</p>
+                        <div className={style.zustandmarke}>
+                            <p className={style.pTag}>Wie neu</p>
+                            <p className={style.pTag}>iPad</p>
+                            <p className={style.pTag}>Ja</p>
+                            <p className={style.pTag}>1 stk.</p>
                         </div>
                     </div>
                     <h3>Produktbeschreibung</h3>
@@ -37,13 +37,14 @@ const ProductDetails = () => {
                 </article>
             </section>
 
-            {/* BUTTON OHNE FUNKTION */}
             <div className={style.btnbottom}>
+
                 <motion.button className={style.registerBtnreverse} onClick={""}
                     whileTap={{ scale: 0.95 }}>Bearbeiten</motion.button>
                 <motion.button className={style.registerBtn} onClick={""}
                     whileTap={{ scale: 0.95 }}>Verkauft</motion.button>
             </div>
+
             <div className={style.footerbottom}><Footer /></div>
         </div >
     )
